2c5c243010f0867f42d66ce307d13d1f1deb59a5
jochem725 <jochemlugtenburg@gmail.com>
2020-04-26 12:50:45 +0200

    Don't check for tmux if tmux is not enabled

--- stop-training.sh
+++ stop-training.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+
+source config.env
+
 export ROBOMAKER_COMMAND=""
 docker-compose -f ./docker-compose.yml down
 
 docker stop $(docker ps | awk ' /sagemaker/ { print $1 }')
 docker rm $(docker ps -a | awk ' /sagemaker/ { print $1 }')
 
+
 if [ "$ENABLE_LOCAL_DESKTOP" = true ] ; then
     wmctrl -c kvs_stream
 fi
 
-if [ ! -z "$(which tmux)" ]
-then
-  tmux kill-session
+if [ "$ENABLE_TMUX" = true ] ; then
+	if [ ! -z "$(which tmux)" ] ; then
+	  tmux kill-session
+	fi
 fi
 
